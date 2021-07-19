a158f59a1a64eff5a5dc876e3164f98797e5a40f
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2021-07-19 17:17:27 -0300

    pages: inserindo mudanças

--- pages/api/personagens.js
+++ pages/api/personagens.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
-import { apiBase, hash, pubKey } from '../../../lib/marvel';
+import { apiBase, hash, pubKey } from '../../lib/marvel';
 
-export default async function handler(req, res) {
+export default async function getPersons(req, res) {
   try {
     const data = await axios.get(`${apiBase}characters?&ts=1&apikey=${pubKey}&hash=${hash}`)
-    res.status(200).json(data.data.data.results)
+    console.log(data);
+    res.status(200).json({
+      list: data.data.data
+    })
   } catch (error) {
     res.status(404)
     console.error(error);
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
+// import Head from 'next/head'
+// import Image from 'next/image'
 import styles from '../styles/css/Home.module.css'
 import axios from 'axios';
 
-export default function Home() {
+const Home = ({ list }) => {
   return (
     <div className={styles.container}>
-      <h1>Hello World</h1>
+      <p>{list.autor[0]}</p>
+      <p>{list.autor[1]}</p>
+      <p>{list.dias[1]}</p>
+      <p>{list.dias[0]}</p>
     </div>
   )
 }
 
+export default Home
 
-export async function getServerSideProps(){
-  const res = await axios.get('http://localhost:3000/api/') 
+
+export async function getStaticProps() {
+  const data = await axios.get('http://localhost:3000/api/personagens');
+
+  return {
+    props: {
+      list: data
+    },
+  };
 }
