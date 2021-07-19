f611c35f9dafe2729041ff1900d6aa0f1f2986b3
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2021-07-19 19:52:01 -0300

    finalizando

--- pages/personagem/[id].jsx
+++ pages/personagem/[id].jsx
@@ -5,6 +5,7 @@ const personagem = ({list}) => {
       <h1>{list[0].name}</h1>
       <h2>{list[0].description}</h2>
       <img src={`${list[0].thumbnail.path}/detail.${list[0].thumbnail.extension}`} alt={`imagem do ${list[0].name}`} />
+      <a href='/'>Inicio</a>
     </div>
   )
 }
@@ -15,7 +16,6 @@ export default personagem
 export async function getServerSideProps(context){
   const {id} = context.params
   const res = await axios.get(`http://localhost:3000/api/SearchById/${id}`)
-  console.log(res.data.list);
   return {
     props : {
       list: res.data.list
