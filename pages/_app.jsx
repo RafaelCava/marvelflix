66ba673d657f1513c238d8f16047b845c34722f4
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2021-07-19 11:49:58 -0300

    pages: trocando extensôes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/css/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/css/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
