d00469237ee2d1e0b40b15d672c49735fe0586e3
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2021-07-20 01:28:41 -0300

    inserindo material ui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,22 +1,6 @@
 import '../styles/css/globals.css'
-import { Button, ThemeProvider, createTheme } from '@material-ui/core';
-
 function MyApp({ Component, pageProps }) {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#000"
-      },
-      secondary: {
-        main:"#3f51b5"
-      }
-    }
-  })
-  return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
