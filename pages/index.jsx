0825de6b884605a57328767718edcbf2b8440331
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2021-07-19 23:17:36 -0300

    estilizando usando sass

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,8 +8,18 @@ const Home = ({ list }) => (
     <Head>
       <title>MarvelFlix</title>
       <link rel="icon" href="/favicon.ico" />
+      <link href="https://fonts.googleapis.com/css?family=Lato:100,100italic,300,300italic,regular,italic,700,700italic,900,900italic" rel="stylesheet" />
     </Head>
-    <main>
+    <header className={styles.header}>
+      <div className="logo">
+        <h1>
+          <span>Marvel</span>
+          Flix
+        </h1>
+      </div>
+      <h2>Os 100 Melhores personagens da Marvel</h2>
+    </header>
+    <main className={styles.main}>
       <ul>
         {list.results.map(item => (
           <li>
